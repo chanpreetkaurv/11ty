044baab34afbcf536ad5da1337bf5b354d222707
Chanpreet Kaur <chanpreet.kaur@nagarro.com>
2022-01-23 22:11:06 +0530

    Added _site folder

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = config => {
         htmlTemplateEngine: 'njk',
         dir: {
             input: 'src',
-            output: 'dist'
+            output: '_site'
         }
     };
 };
